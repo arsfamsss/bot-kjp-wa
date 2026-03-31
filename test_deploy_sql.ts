f6add2b69b689e27bcd241040c4f48e236cffbd9
Fendi <arsfamsss@gmail.com>
2026-04-01 05:30:05 +0700

    chore: hapus file tes yang tidak terpakai

--- test_deploy_sql.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import { createClient } from '@supabase/supabase-js';
-
-// We can't easily run raw DDL via supabase-js without an exec_sql RPC.
-// Let's use fetch directly to the REST API if possible, or just print instructions.
-console.log("Please run the SQL file in Supabase!");
